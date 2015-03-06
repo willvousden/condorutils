c474f63e2d9aff91168a43b0f1789e6f97ddd796
Will Vousden <will@vousden.me.uk>
2015-03-06 12:33:47 +0000

    Initial commit.

---
+++ condorutils/condorutils.py
@@ -0,0 +1,44 @@
+import re
+import os
+
+__all__ = ['getJobs', 'getSubContent', 'getSubArugment', 'getSubArguments', 'getSubCommand']
+
+def getJobs(dagFileName, subFileName):
+    with open(dagFileName) as dag:
+        content = dag.read()
+
+    # Identify the job that relates to this sub file.
+    subFileName = re.escape(os.path.basename(subFileName))
+    matches = re.findall(r'^JOB (\w+) .*{file}$'.format(file=subFileName), content, re.MULTILINE)
+    return [m.strip() for m in matches]
+
+def getSubContent(subFileName):
+    subContent = {}
+    with open(subFileName, 'r') as sub:
+        matches = re.findall(r'^(.+?)\s+=\s+(.+)$', sub.read(), re.MULTILINE)
+        for k, v in matches:
+            subContent[k.strip()] = v.strip()
+    return subContent
+
+def getSubVariables(dagFileName, subFileName):
+    subVariables = {}
+
+    # Identify the job that relates to this sub file.
+    jobs = getJobs(dag, subFileName)
+    assert len(jobs) > 0, 'Couldn\'t find jobs.'
+
+    with open(dagFileName, 'r') as dag:
+        dagContent = dag.read()
+    
+    # Locate and extract the argument key/value pairs.
+    match = re.search(r'^VARS {:}.+$'.format(re.escape(jobs[0])), dagContent, re.MULTILINE)
+    matches = re.findall(r'(\w+)="(.*?)"', match.group(0))
+    for k, v in matches:
+        subVariables[k] = v
+
+def getSubArguments(subContent, subVariables):
+    return re.sub(r'\$\((\w+)\)', lambda m: subVariables[m.group(1)], subContent['arguments'])
+
+def getSubCommand(subContent, subVariables):
+    return '{executable} {arguments}'.format(executable=subVariables['executable'],
+                                             arguments=getSubArguments(subContent, subVariables))
---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+#from codecs import open  # To use a consistent encoding
+#from os import path
+
+#here = path.abspath(path.dirname(__file__))
+#with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    #long_description = f.read()
+
+setup(
+        name='condorutils',
+        version='0.1.0',
+        author='Will Vousden',
+        author_email='will@vousden.me.uk',
+        license='MIT',
+        keywords=['condor'],
+        packages=find_packages(exclude=['docs', 'tests*']),
+        #url='https://github.com/willvousden/condorutils',
+        description='Some utilities for inspecting and manipulating condor files.',
+        long_description=long_description,
+        #install_requires=[]
+)
